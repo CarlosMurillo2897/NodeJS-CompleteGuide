55b67037249f008da1c8cd70221f03b276e1986b
Carlos Murillo <cmb2897@gmail.com>
2025-10-10 12:18:30 -0300

    Using arrow functions.

--- 02-JS-Refresher/playground.js
+++ 02-JS-Refresher/playground.js
@@ -1,12 +1,21 @@
 // var it's considered an outdated syntax.
-const name = "Carlos";
+const userName = "Carlos";
 let age = 28;
 const hasHobbies = true;
 
 age = 30;
 
-function summarizeUser(userName, userAge, userHasHobbies) {
+const summarizeUser = (userName, userAge, userHasHobbies) => {
   return `Name is ${userName}, age is ${userAge} and the user has hobbies: ${userHasHobbies}`;
 }
 
-console.log(summarizeUser(name, age, hasHobbies));
+// const add = (a, b) => a + b;
+// const addOne = a  => a + 1;
+const addRandom = ()  => 2 + 3;
+
+// console.log(add(2, 3));
+// console.log(addOne(4));
+console.log(addRandom());
+
+
+console.log(summarizeUser(userName, age, hasHobbies));
