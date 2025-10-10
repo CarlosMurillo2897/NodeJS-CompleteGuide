526f3e608e713afdb5ee472e4377a42c10ac24b3
Carlos Murillo <cmb2897@gmail.com>
2025-10-10 16:41:48 -0300

    Adding fs module control for a post request.

--- 03-Understanding-Basics/app.js
+++ 03-Understanding-Basics/app.js
@@ -1,7 +1,8 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  const {url} = req;
+  const {url, method} = req;
   if( url === '/') {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
@@ -10,6 +11,14 @@ const server = http.createServer((req, res) => {
     res.write('</html>');
     return res.end();
   }
+
+  if(url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end(); 
+  }
+    
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My First Page</title></head>');
