d335521b5fe9c552fbd5de07cf675478fc825dee
Carlos Murillo <cmb2897@gmail.com>
2025-10-12 17:41:32 -0300

    Using html files for add product and shop routes.

--- 05-Express-JS/routes/shop.js
+++ 05-Express-JS/routes/shop.js
@@ -1,8 +1,11 @@
+const path = require('path');
+
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>'); // Send a response to the client.
+    // dirname is a global, the current directory of this file.
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html')); // Send a response to the client.
 });
 
 module.exports = router;
