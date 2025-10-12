10a0a7f57a3e523c07be309f81ed248de43df0e9
Carlos Murillo <cmb2897@gmail.com>
2025-10-12 17:30:59 -0300

    Adding admin & shop routes.

--- 05-Express-JS/app.js
+++ 05-Express-JS/app.js
@@ -3,19 +3,13 @@ const bodyParser = require('body-parser')
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: false })); // Middleware to parse the body of incoming requests.
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>'); // Send a response to the client.
-});
+app.use('/admin', adminRoutes); // Use the admin routes for any requests to /admin.
+app.use(shopRoutes); // Use the shop routes for any requests to /shop.
 
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/'); // Redirect to another route.
-});
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>'); // Send a response to the client.
-});
 
 app.listen(3000);
