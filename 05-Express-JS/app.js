fbc12508b48c4c5fccb7e50b3f51bc2a846fec8d
Carlos Murillo <cmb2897@gmail.com>
2025-10-12 17:16:17 -0300

    Adding Nodemon, Express and Body-Parser.

--- 05-Express-JS/app.js
+++ 05-Express-JS/app.js
@@ -1,8 +1,21 @@
-const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
 
-const server = http.createServer(app);
+app.use(bodyParser.urlencoded({ extended: false })); // Middleware to parse the body of incoming requests.
 
-server.listen(3000);
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>'); // Send a response to the client.
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/'); // Redirect to another route.
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from Express!</h1>'); // Send a response to the client.
+});
+
+app.listen(3000);
