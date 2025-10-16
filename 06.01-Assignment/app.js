d0caf16c515f9f981c2eb92dd0a3f1690857c1e9
Carlos Murillo <cmb2897@gmail.com>
2025-10-16 20:44:23 -0300

    Creating assignment project with HBS, Pug & EJS engines.

---
+++ 06.01-Assignment/app.js
@@ -0,0 +1,46 @@
+// TODO: Install express, ejs, pug, express-handlebars, body-parser & nodemon as dev.
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
+
+const app = express();
+
+const users = [];
+
+
+// app.engine('hbs', expressHbs({
+//     defaultLayout: 'main-layout',
+//     extname: 'hbs'
+// }));
+
+// app.set('view engine', 'pug');
+// app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.get('/', (req, res, next) => {
+    res.render('index', {
+        pageTitle: 'Add User'
+    });
+});
+
+app.get('/users', (req, res, next) => {
+    res.render('users', {
+        pageTitle: 'Users',
+        users: users,
+        hasUsers: users.length > 0
+    });
+});
+
+app.post('/add-user', (req, res, next) => {
+    users.push({
+        name: req.body.username
+    });
+    res.redirectTo('/users');
+});
+
+app.listen(3000);
