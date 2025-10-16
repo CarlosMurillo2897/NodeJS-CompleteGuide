9f4d056305474269c330d8b6fb15a83315b4de83
Carlos Murillo <cmb2897@gmail.com>
2025-10-16 19:13:46 -0300

    Using View Engine EJS, moving HTML to EJS engine.

--- 06-Dynamic-Content/app.js
+++ 06-Dynamic-Content/app.js
@@ -6,12 +6,7 @@ const expressHbs = require('express-handlebars');
 const app = express();
 
 // TODO: Install ejs, pug, express-handlebars
-app.engine('hbs', expressHbs({
-    layoutsDir: 'views/layouts',
-    defaultLayout: 'main-layout',
-    extname: 'hbs'
-}));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
