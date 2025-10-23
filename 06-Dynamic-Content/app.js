70d8b961f82323c24d02ef51d9c05f85d3dd7281
Murillo Carlos <cmb2897@gmail.com>
2025-10-23 08:52:23 -0600

    Section 06. Fixing 404, moving styles, adding classes and npm pkgs.

--- 06-Dynamic-Content/app.js
+++ 06-Dynamic-Content/app.js
@@ -22,7 +22,8 @@ app.use(shopRoutes); // Use the shop routes for any requests to /shop.
 app.use((req, res, next) => {
     res.status(404)
         .render('404', {
-            pageTitle: 'Page Not Found'
+            pageTitle: 'Page Not Found',
+            path: '404',
         }); // Send a response to the client.
 });
 
