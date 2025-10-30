057171582a0bb3ed391f999639b6aae6199143dd
Murillo Carlos <cmb2897@gmail.com>
2025-10-29 18:14:51 -0600

    Using callbacks to consume single method logic.

--- 07-MVC/models/product.js
+++ 07-MVC/models/product.js
@@ -1,20 +1,24 @@
 const fs = require('fs');
 const path = require('path');
 const rootDir = require('../utils/path');
-
 const p = path.join(rootDir, 'data', 'products.json');
+
+const getProductsFromFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if(err) {
+            return cb([]);
+        }
+        cb(JSON.parse(fileContent));
+    });
+};
 
 module.exports = class Product {
     constructor(t) {
         this.title = t;
     }
 
-    save() {      
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if(!err) {
-                products = JSON.parse(fileContent);
-            }
+    save() {
+        getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), err => {
                 console.log(err);
@@ -23,11 +27,6 @@ module.exports = class Product {
     }
 
     static fetchAll(cb) {
-        fs.readFile(p, (err, fileContent) => {
-            if(err) {
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        });
+        getProductsFromFile(cb);
     }
 };
