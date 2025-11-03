57ee30b341a3b8c5ebd8ba047694ec1bf8635ce7
Murillo Carlos <cmb2897@gmail.com>
2025-11-03 12:21:07 -0600

    Adding get product by ID controller/model and route.

--- 08-Dynamic-Routes/routes/shop.js
+++ 08-Dynamic-Routes/routes/shop.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { getProducts, getIndex, getCart, getCheckout, getOrders } = require('../controllers/shop');
+const { getProducts, getIndex, getCart, getCheckout, getOrders, getProduct } = require('../controllers/shop');
 
 const router = express.Router();
 
 router.get('/', getIndex);
 router.get('/products', getProducts);
+router.get('/products/:productID', getProduct);
 router.get('/cart', getCart);
 router.get('/orders', getOrders);
 router.get('/checkout', getCheckout);
