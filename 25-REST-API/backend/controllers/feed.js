d5230b5807f7d71be52a9c9e00e28607356fb821
Murillo Carlos <cmb2897@gmail.com>
2025-11-18 19:23:39 -0600

    Deleting Post.

--- 25-REST-API/backend/controllers/feed.js
+++ 25-REST-API/backend/controllers/feed.js
@@ -1,4 +1,7 @@
+const fs = require('fs');
+const path = require('path');
 const { validationResult } = require('express-validator');
+
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
@@ -73,4 +76,82 @@ exports.getPost = (req, res, next) => {
       }
       next(err);
     });
+};
+
+exports.updatePost = (req, res, next) => {
+  const errors = validationResult(req);
+  if( !errors.isEmpty() ) {
+    const error = new Error('Validation failed, entered data is incorrect.');
+    error.statusCode = 422;
+    throw error;
+  }
+
+  const { postId } = req.params;
+  let imageUrl = req.body.image;
+  const { title, content } = req.body;
+
+  if (req.file) {
+    imageUrl = req.file.path.replace("\\" ,"/");
+  }
+  if(!imageUrl) {
+     const error = new Error('No file picked.');
+      error.statusCode = 404;
+      throw error;
+  }
+
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('Could not find Post.');
+        error.statusCode = 404;
+        throw error;
+      }
+      if (imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl);
+      }
+      post.title = title;
+      post.imageUrl = imageUrl;
+      post.content = content;
+
+      return post.save()
+        .then(result => {
+            res.status(200).json({
+            message: 'Post updated.',
+            post: result
+          });
+        });
+    }).catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.deletePost = (req, res, next) => {
+  const { postId } = req.params;
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('Could not find Post.');
+        error.statusCode = 404;
+        throw error;
+      }
+      // Check logged in user.
+      clearImage(post.imageUrl);
+      return Post.findByIdAndDelete(postId);
+    }).then(result => {
+      console.log(result);
+      res.status(200).json({ message: 'Deleted Post.'});
+    }).catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+const clearImage = filePath => {
+  filePath = path.join(__dirname, '..', filePath);
+  fs.unlink(filePath, err => console.log(err));
 };
