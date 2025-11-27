44812fa1656d1277b44abb60702797382e819c2d
Carlos Murillo <cmb2897@gmail.com>
2025-11-27 12:34:51 -0600

    Adding socket IO & Client.

--- 27-Sockets/backend/app.js
+++ 27-Sockets/backend/app.js
@@ -59,7 +59,11 @@ app.use((error, req, res, next) => {
 mongoose.connect(
     'mongodb+srv://cmb2808:root@common.b7p2yss.mongodb.net/messages?appName=common'
 ).then(_ => {
-    app.listen(8080);
+    const server = app.listen(8080);
+    const io = require('./socket').init(server);
+    io.on('connection', socket => {
+        console.log('Client connected');
+    });
 }).catch(err =>{
     console.log(err);
 });
